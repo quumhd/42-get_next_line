ed63bb99ec5383099534cb418f8645e72915c6f0
Jens D <jdreissi@3-H-8.42heilbronn.de>
2025-11-03 20:06:58 +0100

    only reading one byte at a time

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jdreissi <jdreissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 12:33:35 by jdreissi          #+#    #+#             */
-/*   Updated: 2025/11/02 16:24:17 by jdreissi         ###   ########.fr       */
+/*   Updated: 2025/11/03 19:48:20 by jdreissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,8 @@
 char	*get_next_line(int fd);
 
 size_t	ft_strlen(const char *s);
-size_t	find_newline(char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_strndup(const char *s, int length);
-char	*get_line(char *buffer);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strdup(const char *s);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,72 +6,42 @@
 /*   By: jdreissi <jdreissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 12:31:07 by jdreissi          #+#    #+#             */
-/*   Updated: 2025/11/02 16:27:23 by jdreissi         ###   ########.fr       */
+/*   Updated: 2025/11/03 19:47:27 by jdreissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	length;
-
-	length = 0;
-	while (s[length])
-		length++;
-	return (length);
-}
-
-size_t	find_newline(char *s)
-{
-	size_t index;
-
-	index = 0;
-	while(s[index] != '\0')
-	{
-		if(s[index] == '\n')
-			return (index);
-		index++;
-	}
-	return (0);
-}
-
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	j;
+	size_t	startdest;
 
-	i = -1;
-	if (size == 0)
-		return (ft_strlen(src));
-	while (++i < size - 1 && src[i])
-		dest[i] = ((char *)src)[i];
+	i = ft_strlen(dest);
+	startdest = i;
+	j = 0;
+	if (i >= size)
+		return (size + ft_strlen(src));
+	while (i < size - 1 && src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = 0;
-	return (ft_strlen(src));
+	return (ft_strlen(src) + startdest);
 }
 
-char	*ft_strndup(const char *s, int length)
+char	*ft_strdup(const char *s)
 {
+	size_t	length;
 	char	*duplicated;
 
-	duplicated = malloc(length * sizeof(char));
+	length = ft_strlen(s) + 1;
+	duplicated = (char *)malloc((length) * sizeof(char));
 	if (!duplicated)
-		return (NULL);
+		return ((void *)0);
 	ft_strlcpy(duplicated, s, length);
 	return (duplicated);
-}
-
-char	*get_line(char *buffer)
-{
-	int		index;
-	char	*line;
-
-	index = find_newline(buffer);
-	if(index != 0)
-		return (ft_strndup(buffer, index + 2));
-	while(find_newline == 0)
-	{
-		line = malloc(ft_strlen(buffer) * sizeof(char));
-		
-	}
-	return (NULL);
 }
